7cea063d8b3bdfa17fdc22acd1d479f05ba40228
Ruan <oliveiraruan2018@gmail.com>
2025-05-29 15:22:03 -0400

    chore: application use dir public

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 import routes from './routes.js';
-
 class App {
 	constructor() {
 		this.server = express();
@@ -12,6 +12,10 @@ class App {
 
 	middlewares() {
 		this.server.use(express.json());
+
+		const _filename = fileURLToPath(import.meta.url);
+		const _dirname = path.dirname(_filename);
+		this.server.use(express.static(path.join(_dirname, '../public')));
 	}
 
 	routes() {
