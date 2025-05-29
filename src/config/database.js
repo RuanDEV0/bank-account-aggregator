fc2c99b709bb723e40c2ef5f04349005522a4b81
Ruan <oliveiraruan2018@gmail.com>
2025-05-29 15:23:56 -0400

    fix: host database enviroment in .env

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 
 export default{
 	dialect: 'postgres',
-	host: 'db',
+	host: process.env.DB_HOST,
 	username: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
